67ec135dd9b0e70f74a309b99c43c566a4367e30
Aditya Vikram <vikadia7x@gmail.com>
2018-02-21 17:34:04 +0530

    Add files via upload

---
+++ churn_modelling_keras.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 14:21:56 2018
+
+@author: Aditya Vikram
+"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#Now its time to use ANN
+#We start by importing keras
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+#Sequential is used to initialise the ANN - defining it as a sequence of layers whereas Dense is used to add layers to the ANN
+
+#Lets start with initialising the ANN - We use a sequence to initialise
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+# Part 3 - Making predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#If we want to make a new prediction, we input the new prediction in an np arrray.
+#Geography: France
+#Credit Score: 600
+#Gender: Male
+#Age: 40 years old
+#Tenure: 3 years
+#Balance: $60000
+#Number of Products: 2
+#Does this customer have a credit card ? Yes
+#Is this customer an Active Member: Yes
+#Estimated Salary: $50000
+
+new_pred = np.array([[0,0,619,0,40,3,60000,2,1,1,50000]])
+new_pred = sc.transform(new_pred)
+
+y_new_pred = classifier.predict(new_pred)
+y_new_pred = (y_new_pred>0.5)
